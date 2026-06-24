ca6842b76c9402d2b5260b1cb0fcb6fd18fb99fc
Mahanthi Leela Krishna Prasad <mahanthileelakrishnaprasad@gmail.com>
2026-06-25 02:21:47 +0530

    Add PYTHONPATH to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
+
+# This line is missing and is required to fix the ModuleNotFoundError:
+export PYTHONPATH=.
+
 python manage.py collectstatic --no-input
 python manage.py migrate
