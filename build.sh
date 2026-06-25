9e902aa55d0917d60115cf5cc13a3790866df055
Mahanthi Leela Krishna Prasad <mahanthileelakrishnaprasad@gmail.com>
2026-06-25 05:49:28 +0530

    Fix manage.py path in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
-python taskmaster/manage.py collectstatic --no-input
-python taskmaster/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
